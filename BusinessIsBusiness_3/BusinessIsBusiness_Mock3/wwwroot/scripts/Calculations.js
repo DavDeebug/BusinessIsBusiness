0cc39108ce40e0a911af969b6d850a19d337018c
Davide Corrain <33847294+dc0rrain@users.noreply.github.com>
2018-03-05 20:49:05 +0100

    Add files via upload
    First commit - dropdown list populated from an array of objects, definition of calculations, modal dialog-window

---
+++ BusinessIsBusiness_3/BusinessIsBusiness_Mock3/wwwroot/scripts/Calculations.js
@@ -0,0 +1,58 @@
+﻿function CalculateQuotation(product) {
+    //var productType // la classe dell'elemento selezionato 
+    //var unitPrice = SelectedItem.unitPrice;
+    var discountValue = $("#discount").val();
+
+    var totalQuantity = GetTotalQuantity(productType);
+    var totalPrice = GetTotalPrice(unitPrice, totalQuantity);
+    var quotationPrice = GetQuotationPrice(totalPrice, discountValue);
+
+    
+}
+
+function GetTotalQuantity(productType) {
+    var totalQuantity;
+    switch (productType) {
+        case "volume":
+            totalQuantity = getVolumeValue();
+            break;
+        case "area":
+            totalQuantity = getAreaValue();
+            break;
+        case "pezzo":
+            totalQuantity = getAmount();
+            break;
+    }
+    $("#totalQuantity").val(totalQuantity);
+    return totalQuantity;
+}
+
+function GetTotalPrice(price, quantity) {
+    var tp = price * quantity;
+    $("#totalPrice").val(tp);
+    return tp;
+}
+
+function GetQuotationPrice(total, discount) {
+    var fp = total - discount;
+    $("#finalPrice").val(fp);
+    return fp;
+}
+
+function getVolumeValue() {
+    var l = $("#volumeLength").val();
+    var h = $("#volumeHeight").val();
+    var w = $("#volumeWidth").val();
+    return l * h * w;
+}
+
+function getAreaValue() {
+    var l = $("areaLength").val();
+    var h = $("areaHeight").val();
+    return l * h;
+}
+
+function getAmount() {
+    var i = $("#quantity").val();
+    return +i;
+}
---
+++ BusinessIsBusiness_3/BusinessIsBusiness_Mock3/wwwroot/scripts/catalogue.js
@@ -0,0 +1,59 @@
+﻿var volumeProducts = [
+    { ProductName: "Prova1", UnitPrice: 7 },
+    { ProductName: "Prova2", UnitPrice: 5 },
+    { ProductName: "Prova3", UnitPrice: 2 },
+    { ProductName: "Prova4", UnitPrice: 4 }
+]
+var areaProducts = [
+    { ProductName: "Prova5", UnitPrice: 7 },
+    { ProductName: "Prova6", UnitPrice: 7 },
+    { ProductName: "Prova7", UnitPrice: 3 }
+]
+
+var quantityProducts = [
+    { ProductName: "Prova8", UnitPrice: 18 },
+    { ProductName: "Prova9", UnitPrice: 10 },
+    { ProductName: "Prova0", UnitPrice: 1 }
+]
+
+
+
+var catalogueProducts = [
+    { Name: "volume", Data: volumeProducts },
+    { Name: "area", Data: areaProducts },
+    { Name: "pezzo", Data: quantityProducts }]
+
+// popoliamo il dropdown
+function PopulateDropDown() {
+
+
+    var content = $('ul.dropdown-menu')
+    $.each(catalogueProducts, function (i) {
+        var headLi = $('<li/>')
+            .addClass('dropdown-header')
+            .text('Raggruppamento per ' + catalogueProducts[i].Name)
+            .appendTo(content);
+
+        $.each(catalogueProducts[i].Data, function (num) {
+            var li = $('<li/>')
+                .appendTo(content);
+            var aaa = $('<a/>')
+                .addClass(catalogueProducts[i].Name)
+                .attr('data-target', '#prova')
+                .attr('data-toggle', 'modal')
+                .attr('href', '#')
+                // provo ad aggiungere un id
+                .attr('id', (catalogueProducts[i].Name + "Item" + num))
+                .text(catalogueProducts[i].Data[num].ProductName)
+                .appendTo(li);
+        })
+
+        if (i != catalogueProducts.length) {
+            var divider = $('<li/>')
+                .addClass('divider')
+                .appendTo(content);
+        }
+
+    });
+
+}
